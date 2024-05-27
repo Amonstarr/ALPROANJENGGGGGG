75eafa84bbb0dc7d29805f1f2eb4e7bd10f40299
Amonstarr <161749247+Amonstarr@users.noreply.github.com>
2024-05-27 23:15:01 +0700

    delete old main

--- ternakbuaya.c
+++ ternakbuaya.c
@@ -37,56 +37,6 @@ int itemCount = 3;
 Member members[MAX_MEMBERS];
 int memberCount = 0;
 
-int main() {
-    loadMembers();
-    loadItems();
-
-    int choice;
-    while (1) {
-        printf("\n========================================================\n");
-        printf("------------------------- COOFARM ----------------------\n");
-        printf("========================================================\n");
-        printf("1. Tambah Anggota\n");
-        printf("2. Ajukan Pinjaman\n");
-        printf("3. Beli Barang Ternak\n");
-        printf("4. Lihat Anggota\n");
-        printf("5. Cek Saldo\n");
-        printf("6. Pembayaran\n");
-        printf("7. Keluar\n");
-        printf("Pilih menu: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                addMember();
-                break;
-            case 2:
-                applyLoan();
-                break;
-            case 3:
-                buyItem();
-                break;
-            case 4:
-                viewMember();
-                break;
-            case 5:
-                checkBalance();
-                break;
-            case 6:
-                makePayment();
-                break;
-            case 7:
-                saveMembers();
-                saveItems();
-                exit(0);
-            default:
-                printf("Pilihan tidak valid!\n");
-        }
-    }
-
-    return 0;
-}
-
 void menuMember() 
 {
     loadMembers();
