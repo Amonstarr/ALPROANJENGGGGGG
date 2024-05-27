9337f7ed5702e07fa6d840c864875304eb962d43
Amonstarr <161749247+Amonstarr@users.noreply.github.com>
2024-05-27 22:55:28 +0700

    tambah struct

--- ternakbuaya.c
+++ ternakbuaya.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define MAX_MEMBERS 100
+#define MAX_ITEMS 100
+#define MEMBER_FILE "members.dat"
+#define ITEM_FILE "items.dat"
+#define INITIAL_DEPOSIT 50000.0
+
+typedef struct {
+    char name[50];
+    float amount;
+    int duration; // in months
+} Loan;
+
+typedef struct {
+    char name[50];
+    float price;
+    int quantity;
+} Livestock;
+
+typedef struct {
+    char name[50];
+    float balance;
+    Loan loans[MAX_MEMBERS];
+    int loanCount;
+} Member;
+
+Livestock items[MAX_ITEMS] = {
+    {"Ayam", 50000.0, 100},
+    {"Kambing", 1500000.0, 50},
+    {"Sapi", 10000000.0, 20}
+};
+int itemCount = 3;
+
+Member members[MAX_MEMBERS];
+int memberCount = 0;
