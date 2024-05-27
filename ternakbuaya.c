041f93f5fac043567abc8e7f8e24f6454c32181b
Amonstarr <161749247+Amonstarr@users.noreply.github.com>
2024-05-28 00:06:04 +0700

    Update

--- ternakbuaya.c
+++ ternakbuaya.c
@@ -18,19 +18,24 @@ typedef struct {
     char name[50];
     float price;
     int quantity;
-} Livestock;
+} item;
 
 typedef struct {
     char name[50];
+    long int id;
     float balance;
     Loan loans[MAX_MEMBERS];
     int loanCount;
 } Member;
 
-Livestock items[MAX_ITEMS] = {
-    {"Ayam", 50000.0, 100},
-    {"Kambing", 1500000.0, 50},
-    {"Sapi", 10000000.0, 20}
+item items[MAX_ITEMS] = {
+    {"Pakan ayam", 20000.0, 80},
+    {"Pakan Kambing", 50000.0, 50},
+    {"Pakan Sapi", 80000.0, 20},
+    {"Telur Ayam", 18000.0, 100},
+    {"Daging Ayam", 35000.0, 20},
+    {"Daging Kambing", 65000.0, 20},
+    {"Daging Sapi", 105000.0, 20},
 };
 int itemCount = 3;
 
@@ -39,7 +44,7 @@ int memberCount = 0;
 
 void menuMember() 
 {
-    loadMembers();
+    //loadMembers();
     loadItems();
 
     int choice;
@@ -47,11 +52,11 @@ void menuMember()
     printf("========================================================\n");
     printf("-------------------------- MEMBER ----------------------\n");
     printf("========================================================\n");
-    printf("2. Ajukan Pinjaman\n");
-    printf("3. Beli Barang Ternak\n");
-    printf("5. Cek Saldo\n");
-    printf("6. Pembayaran\n");
-    printf("7. Keluar\n");
+    printf("1. Ajukan Pinjaman\n");
+    printf("2. Beli Barang\n");
+    printf("3. Cek Saldo\n");
+    printf("4. Pembayaran\n");
+    printf("5. Kembali\n");
     printf("Pilih menu: ");
     scanf("%d", &choice);
 
@@ -74,6 +79,7 @@ void menuMember()
         break;
 
         case 5:
+        main();
         break;
 
         default:
@@ -91,7 +97,7 @@ void employee()
     printf("1. Tambah Anggota\n");
     printf("2. Lihat Anggota\n");
     printf("3. Lihat Penjualan\n");
-    printf("4. Keluar\n");
+    printf("4. Kembali\n");
     printf("Pilih menu: ");
     scanf("%d", &choice);
     getchar();
@@ -112,6 +118,7 @@ void employee()
         break;
 
         case 4:
+        main();
         break;
 
         default:
@@ -129,7 +136,7 @@ int main()
     printf("========================================================\n");
     printf("1. Pegawai\n");
     printf("2. Pelanggan\n");
-    printf("7. Keluar\n");
+    printf("3. Keluar\n");
     printf("Pilih menu: ");
     scanf("%d", &choice);
     getchar();
@@ -138,11 +145,11 @@ int main()
     switch (choice)
     {
         case 1:
-        menuMember();
+        employee();
         break;
 
         case 2:
-        employee();
+        menuMember();
         break;
 
         case 3:
@@ -153,5 +160,14 @@ int main()
 
         default:
         printf("Pilihan tidak valid!\n");
+    }
+}
+
+void loadItems() {
+    FILE *file = fopen(ITEM_FILE, "rb");
+    if (file) {
+        fread(&itemCount, sizeof(int), 1, file);
+        fread(items, sizeof(item), itemCount, file);
+        fclose(file);
     }
 }
